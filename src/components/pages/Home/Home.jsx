c2dfd6fa1116adf165ab3c8d7f2b67d703c3a356
PabloDantasDev <contato.pablodantasdev@gmail.com>
2022-03-28 10:18:42 -0300

    Atualzando Projeto

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -11,9 +11,11 @@ function Home() {
             <Link to='/'><img className={style.logo} src={home} alt="Costs Logo" /></Link>
             <div className={style.titleSub}>
                 <h1>COSTS - PLANEJAMENTO E FINANÇAS</h1>
-                <h3>Costs foi pensado para todos aqueles que busca um lugar onde possa organizar sua vidafinanceira em poucos cliques!</h3>     
-                 <div className={style.btn}>
-                 < Link className={style.btnHome} to='/project'> COMECE POR AQUI! </Link>
+                <h3>Costs foi pensado para todos aqueles que busca um lugar onde possa organizar sua vida financeira em poucos cliques!</h3>     
+                
+                <div className={style.btn}>
+                 
+                 <a href="/project" className={style.btnHome}>CRIAR PROJETO</a>
                  </div>
             </div>
 
